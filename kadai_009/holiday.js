fb2360ee7a954884e1174caf3a6f5904f9036576
mana07 <135534753+mana07@users.noreply.github.com>
2023-06-25 22:08:12 +0900

    Add files via upload

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,13 +1,14 @@
 let holidays=['正月','成人の日','建国記念日','天皇誕生日','春分の日','昭和の日','憲法記念日','みどりの日','こどもの日','海の日','山の日','敬老の日','秋分の日','スポーツの日','文化の日','勤労感謝の日'];
 
-console.log(holidays);
+let index=0;
 
-while(holidays<holidays.length){
-  
-  console.log(holidays[i]);
+while(index <holidays.length){
+  console.log(holidays[index]);
+  index++;
 }
-
 
 for(let i=0;i<holidays.length;i++){
   console.log(holidays[i]);
 }
+
+
