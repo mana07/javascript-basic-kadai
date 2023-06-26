7c711312378cb6e95267db488a6f0fb2b28feffd
mana07 <135534753+mana07@users.noreply.github.com>
2023-06-26 16:05:33 +0900

    Kadai_020アップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,14 @@
+const btn=document.getElementById('btn');
+
+btn.addEventListener('click',()=>{
+  setTimeout(()=>{
+    
+  const text=document.getElementById('text').innerHTML='ボタンをクリックしました';
+
+},2000);
+}
+
+);
+
+
+
